8b48163411722c6f54226ce55f1b91180710cad1
Christian rios <cgrios@email.sc.edu>
2021-11-02 14:12:22 -0400

    fixed JobListing

--- JobListing.java
+++ JobListing.java
@@ -1,18 +1,26 @@
-<<<<<<< HEAD
-import java.util.ArrayList;
-=======
 /**
  * @author Anton, Christian, Kylie, Jack
  */
->>>>>>> eec244a5df9b4331eb6a2d27bb93c9800ba8df23
-
+import java.util.ArrayList;
 public class JobListing {
-
-    public JobListing(String location, Double pay, Boolean remote, String date, String description) {
+    private static JobListing joblistings = null;
+    private static ArrayList<Job> jobList = new ArrayList<>();
+    private JobListing() {
+        jobList = DataLoader.getJobListings();
     }
-
-    public ArrayList<Job> searchJobs() {
-        
+    public static JobListing getInstance() {
+        if (joblistings == null) {
+            joblistings = new JobListing();
+        }
+        return joblistings;
     }
-
+    public ArrayList<Job> getJob() {
+        return jobList;
+    }
+    public void addJob(String location, Double pay, Boolean remote, String date, String description) {
+        jobList.add(new Job(location, pay, remote, date, description));
+    }
+    public void logout() {
+        DataWriter.saveJobListing();
+    }
 }
