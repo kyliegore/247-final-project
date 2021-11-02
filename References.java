680c5ca80a5c2f570371ef68a959b9674912e381
Christian rios <cgrios@email.sc.edu>
2021-11-02 13:37:11 -0400

    Fixed references

--- References.java
+++ References.java
@@ -8,10 +8,12 @@ public class References {
     private String email;
     //in our UML but no implementation yet
     public References(String name, String number, String email) {
-
+        this.name = name;
+        this.number = number;
+        this.email = email;
     }
     public String toString() {
-        return " ";
+        return "Name: " + name + "\n" + "Number: " + number + "\n" + "Email: " + email + "\n";
         
         
     }
