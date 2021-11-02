95dbfb5b4c75c18a141d1b4e90e0474c70cb9c1a
Christian rios <cgrios@email.sc.edu>
2021-11-02 16:54:10 -0400

    Fixed Work Experience

--- DataConstants.java
+++ DataConstants.java
@@ -12,7 +12,7 @@ public abstract class DataConstants {
     protected static final String AWARDS = "awards";
     protected static final String EXTRACURRICULARS = "extracurriculars";
     protected static final String REFERENCES = "references";
-    protected static final String EXPERIENCE = "experience";
+    protected static final String EXPERIENCE = "work experience";
 
 
 
