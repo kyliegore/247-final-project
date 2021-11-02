015cc97276acaaf42a4a94656ce8a82690e0d6a4
Anton Aarnink <antonaarnink@MacBook-Pro-4.local>
2021-11-02 14:17:06 -0400

    login

--- Students.java
+++ Students.java
@@ -27,5 +27,17 @@ public class Students {
     public void logout() {
         DataWriter.saveStudent();
     }
+    public Student getStudent(String userName) {
+        return null;
+    }
+    public boolean haveStudent(String userName) {
+         for(Student student: studentList ) {
+                if(student.getUsername().equals(userName)) {
+                    return true;
+                }
+            }
+            return false;
+        
+    }
     
 }
