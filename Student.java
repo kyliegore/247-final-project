7fefd570c04dfcd614c010d29b7a4ad1552c2124
Anton Aarnink <antonaarnink@MacBook-Pro-4.local>
2021-11-02 22:40:55 -0400

    nothing changed

--- Student.java
+++ Student.java
@@ -137,9 +137,9 @@ public class Student   {
         Resume currentResume = new Resume(firstName, lastName, email, number, skills, education, experience);
         resumes.add(currentResume);
     }
-    public void printResume(ArrayList<String> skills, ArrayList<String> education, ArrayList<String> experience) {
+    public void printResume() {
         for(Resume resume : resumes) {
-            resume.printResume();
+            System.out.println(resume.printResume());
         }
     }
     public boolean haveResume() {
