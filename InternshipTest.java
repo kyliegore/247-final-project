79887cc8080c64d6cbe85eeab02440424948b7c3
Christian rios <cgrios@email.sc.edu>
2021-11-14 18:44:24 -0500

    added setup for each test

--- InternshipTest.java
+++ InternshipTest.java
@@ -3,5 +3,21 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 class InternshipTest {
-    
+    public static void setup() {
+        //runs before each test
+    }
+
+    public static void tearDown() {
+        //runs after each test
+    }
+
+    //assertEquals(val1,val2)
+    //assertFalse(val)
+    //assertTrue(val)
+    //assertSame(val1,val2)
+    //assertNotSame(val1,val2)
+    //assertNull(val)
+    //assertNotNull(val)
+
+    @Test
 }
