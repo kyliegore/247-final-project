46db865b7c62130d8a2c8c5717282cb0b5b86b55
Kylie Gore <kylie.a.gore@gmail.com>
2021-11-14 21:18:50 -0500

    employerstest

--- EmployersTest.java
+++ EmployersTest.java
@@ -34,4 +34,32 @@ class EmployersTest {
         ArrayList<Employer> employerList = Employers.getEmployers();
         assertNotNull(employerList);
     }
+
+    @Test
+    public void testGetEmployerThatExists() {
+        setup();
+        Employer employer = Employers.getEmployer("abcd1234");
+        assertNotNull(employer);
+    }
+
+    @Test
+    public void testGetEmployerThatDoesNotExist() {
+        setup();
+        Employer employer = Employers.getEmployer("notinlist");
+        assertNull(employer);
+    }
+
+    @Test
+    public void testHaveEmployerThatExists() {
+        setup();
+        Boolean have = Employers.haveEmployer("abcd1234");
+        assertTrue(have);
+    }
+
+    @Test
+    public void testHaveEmployerThatDoesNotExist() {
+        setup();
+        Boolean have = Employers.haveEmployer("notinlist");
+        assertFalse(have);
+    }
 }
