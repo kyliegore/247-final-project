6636234952b7438842c484d03a7c06b00939ef49
Christian rios <cgrios@email.sc.edu>
2021-11-14 20:29:10 -0500

    Fixed tests of most classes

--- StudentsTest.java
+++ StudentsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.UUID;
 //Tested by Christian Rios
+//Note that these tests come up with an error when testing from the entire directory, but will work when tested individually.
 class StudentsTest {
     public static void setup() {
         //runs before each test
